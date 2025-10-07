fbd3b656b92529625236c1d8508c4740bc77f94f
al-ameen244 <abdulrahmon@joinramp.co>
2025-10-07 22:23:51 +0100

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/login";
+import SignUp from "./pages/auth/signUp";
+import ManageUsers from "./pages/Admin/ManageUsers";
+import Dashboard from "./pages/Admin/Dashboard";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import CreateTask from "./pages/Admin/CreateTask";
+import UserDashboard from "./pages/User/UserDashboard";
+import MyTasks from "./pages/User/MyTasks";
+import ViewTaskDetails from "./pages/User/ViewTaskDetails";
+import PrivateRoute from "./routes/privateRoutes";
 
 const App = () => {
-  return <div className="bg-gray-800 text-white p-4"> App</div>;
+  return (
+    <div className="bg-gray-800 text-white p-4">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+            <Route path="/admin/users" element={<ManageUsers />} />
+          </Route>
+
+          {/* User Routes */}
+          <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route path="/user/task-details" element={<ViewTaskDetails />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/User/ViewTaskDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewTaskDetails = () => {
+  return <div>View Task Details</div>;
+};
+
+export default ViewTaskDetails;
